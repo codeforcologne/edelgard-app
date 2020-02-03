596e010fb5b26cda25b62819c445328d21e7021a
Ansgar Prause <4215065+paqe@users.noreply.github.com>
2020-02-03 17:11:22 +0100

    Add caching service worker

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import Sentry from "./api/sentry";
+import * as ServiceWorker from "./serviceWorker";
+
+ServiceWorker.register();
 
 const sentryDsn = process.env.REACT_APP_SENTRY_DSN;
 if (sentryDsn && process.env.NODE_ENV === "production") {
